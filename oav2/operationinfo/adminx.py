331beb2e020c1d83eceee5919b210057bf35b781
hyf-home-centos7 <hyf-home-centos7@163.com>
2018-09-25 22:52:08 +0800

    20180925晚

--- oav2/operationinfo/adminx.py
+++ oav2/operationinfo/adminx.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 import xadmin
 from .models import *
-
-
+from xadmin.layout import Main, Side, Fieldset, Row, AppendedText#引入xadmin 的fields及fieldset设置模块
+from xadmin.views import BaseAdminPlugin, ModelFormAdminView, DetailAdminView, filter_hook
 # Register your models here.
 
 #1显示供应商
@@ -17,7 +17,13 @@ class ContactsAdmin(object):
     list_display = ('area', 'supplier_name', 'contact_full_name', 'fixed_telephone', 'phone_number')
     exclude = ('isdelete',)  # 排除显示isdelede字段出来
 
+
 xadmin.site.register(ContactsTable, ContactsAdmin)
+
+# class ContactsInline(xadmin.StackedInline):
+#     models = ContactsTable
+#     extra = 1
+
 
 
 #3注册光纤网络表
@@ -35,3 +41,42 @@ class SoftResourceLibraryAdmin(object):
 
 
 xadmin.site.register(SoftResourceLibraryTable, SoftResourceLibraryAdmin)
+
+
+#5注册硬件资源库
+class HardwareResourceLibratyAdmin(object):
+    list_display = ('Fixed_assets_number', 'brand', 'category', 'model')
+    exclude = ('isdelete', 'bak1', 'bak2', 'bak3', 'bak4')  # 排除显示isdelede/bak1/bak2/bak3/bak4字段出来
+
+
+xadmin.site.register(HardwareResourceLibratyTable, HardwareResourceLibratyAdmin)
+
+
+
+#test注册
+class HeroAdmin(object):
+    list_display = ('name', 'gender' , 'hbook' )
+
+
+xadmin.site.register(Heroinfo, HeroAdmin)
+
+
+class Heroinline(object):
+    model = Heroinfo
+    extra = 0
+
+
+class BookAdmin(object):
+    list_display = ('btitle', 'binfo')
+
+    inlines = [Heroinline]
+
+
+
+    form_layout = (
+        Fieldset("书本",
+            Row('btitle', 'binfo')
+        ),
+    )
+
+xadmin.site.register(Bookinfo, BookAdmin)
--- oav2/operationinfo/models.py
+++ oav2/operationinfo/models.py
@@ -91,6 +91,7 @@ class SoftResourceLibraryTable(models.Model):
     department = models.ForeignKey('employeeinfo.DepartmentTable', verbose_name="部门", on_delete=models.CASCADE)
 
 
+
     def __str__(self):
         return self.category
 
@@ -98,4 +99,67 @@ class SoftResourceLibraryTable(models.Model):
 
     class Meta:
         verbose_name = "软件资源库"
+        verbose_name_plural = verbose_name
+
+
+#5硬件资源库
+class HardwareResourceLibratyTable(models.Model):
+    Fixed_assets_number = models.CharField(verbose_name="固定资产", max_length=50)
+    brand = models.CharField(verbose_name="品牌", max_length=50)
+    category = models.CharField(verbose_name="类型", max_length=50)
+    model = models.CharField(verbose_name="型号", max_length=50)
+    purchase_date = models.DateField(verbose_name="采购日期")
+    guarantee =models.DateField(verbose_name="保修日期")
+    service_code = models.CharField(verbose_name="服务代码", max_length=50)
+    cpu = models.CharField(verbose_name="cpu", max_length=50)
+    memory = models.CharField(verbose_name="内存", max_length=50)
+    hard_disk = models.CharField(verbose_name="硬盘", max_length=50)
+    remarks = models.TextField(verbose_name="备注", max_length=1000)
+    area = models.ForeignKey('employeeinfo.AreaTable',verbose_name="区域", on_delete=models.CASCADE)
+    company = models.ForeignKey('employeeinfo.CompanyTable', verbose_name="公司", on_delete=models.CASCADE)
+    department = models.ForeignKey('employeeinfo.DepartmentTable', verbose_name="部门", on_delete=models.CASCADE)
+
+    isdelete = models.BooleanField(default=0)
+    bak1 = models.BooleanField(default=0)
+    bak2 = models.BooleanField(default=0)
+    bak3 = models.BooleanField(default=0)
+    bak4 = models.BooleanField(default=0)
+
+    def __str__(self):
+        return self.Fixed_assets_number
+
+    class Meta:
+        verbose_name = "硬件资源库"
+        verbose_name_plural = verbose_name
+
+
+#6运维信息表
+# class OperationinfoTable(models.Model):
+#     area = models.ForeignKey('employeeinfo.AreaTable', verbose_name="区域", on_delete=models.CASCADE)
+#     company = models.ForeignKey('employeeinfo.CompanyTable', verbose_name="公司", on_delete=models.CASCADE)
+#     department = models.ForeignKey('employeeinfo.DepartmentTable', verbose_name="部门", on_delete=models.CASCADE)
+#     name = models.ForeignKey('employeeinfo.EmployeeTable',)
+
+
+#test表
+
+class Bookinfo(models.Model):
+    btitle = models.CharField("书名", max_length=50)
+    binfo = models.CharField("描述", max_length=50)
+
+    def __str__(self):
+        return self.btitle
+
+
+
+class Heroinfo(models.Model):
+    name = models.CharField("姓名", max_length=50)
+    gender = models.CharField("性别", max_length=50)
+    hbook = models.ForeignKey(Bookinfo, verbose_name="书名", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "英雄"
         verbose_name_plural = verbose_name
